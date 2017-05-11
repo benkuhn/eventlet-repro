3392a97c3bf536e8da61f54bd351a49c2f16cbe5
Ben Kuhn <ben.s.kuhn@gmail.com>
2017-05-11 17:52:14 -0400

    More minimal repro

--- test_eventlet.py
+++ test_eventlet.py
@@ -6,7 +6,9 @@ import threading
 import newrelic.core.application
 
 def dothing():
-    newrelic.core.application.Application('test').activate_session()
+    #newrelic.core.application.Application('test').activate_session()
+    import newrelic.packages.requests.packages.urllib3.util.ssl_ as ssl_
+    ssl_.create_urllib3_context(ssl_.ssl.PROTOCOL_TLS, ssl_.ssl.CERT_REQUIRED)
     print('ran')
 
 logging.basicConfig()
